e628fffa93eeab36460455355a46059d6ec7df1e
ZemljakM <93863336+ZemljakM@users.noreply.github.com>
2023-05-10 14:12:10 +0200

    Update zadatak_3.py

--- LV2/zadatak_3.py
+++ LV2/zadatak_3.py
@@ -1,26 +1,27 @@
 import numpy as np
-import matplotlib . pyplot as plt
-img = plt . imread ("road.jpg")
-img = img [ :,:,0]. copy ()
-plt . imshow ( img , cmap ="gray")
-plt . show ()
+import matplotlib.pyplot as plt
+img = plt.imread ("road.jpg")
+img = img [:,:,0]. copy ()
+plt.imshow( img , cmap ="gray")
+plt.show ()
 
 
 #a
-plt . imshow ( img, vmin=0, vmax=100, cmap ="gray")
-plt . show ()
+plt.imshow ( img, vmin=0, vmax=100, cmap ="gray")
+# drugi nacin:     bright_img=np.clip(img.astype(np.int64)+150,0,255)
+plt.show ()
 
 #b
-cutout=img[:,160:320]
-plt . imshow ( cutout, cmap ="gray")
-plt . show ()
+cutout=img[:,160:320]                   # img[:, (img.shape[1]/4)/(img.shape[1]/2)]
+plt.imshow ( cutout, cmap ="gray")
+plt.show()
 
 #c
-rotation=np.rot90(img)
-plt . imshow ( rotation, cmap ="gray")
-plt . show ()
+rotation=np.rot90(img,3)
+plt.imshow( rotation, cmap ="gray")
+plt.show()
 
 #d
 flip=np.fliplr(img)
-plt . imshow ( flip, cmap ="gray")
-plt . show ()
+plt.imshow( flip, cmap ="gray")
+plt.show()
