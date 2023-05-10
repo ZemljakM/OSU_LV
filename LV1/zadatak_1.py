dc898d269abbde948a69a500aaca4fef0969df0b
ZemljakM <93863336+ZemljakM@users.noreply.github.com>
2023-05-10 13:42:54 +0200

    Update zadatak_1.py

--- LV1/zadatak_1.py
+++ LV1/zadatak_1.py
@@ -1,7 +1,11 @@
 def total_euro(sati, zarada_po_satu):
     return sati*zarada_po_satu
 
-
-x=float(input("Broj radnih sati: "))
-y=float(input("Zarada po satu: "))
-print("Ukupna zarada: ", total_euro(x,y))
+while(1):
+    try:
+        x=float(input("Broj radnih sati: "))
+        y=float(input("Zarada po satu: "))
+        print("Ukupna zarada: ", total_euro(x,y))
+        break
+    except:
+        print("Upisi broj")
